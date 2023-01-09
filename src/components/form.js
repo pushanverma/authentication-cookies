424b8cec8e25161263e375ec5ab636e54addc28d
vermapushan25 <pushanverma.99@gmail.com>
2023-01-09 22:38:36 +0530

    adding files

--- src/components/form.js
+++ src/components/form.js
@@ -1,29 +1,22 @@
 import React,{useState,useEffect} from 'react';
 import userdetails from "../constants/user";
 import Cookies from 'universal-cookie';
-
+import Secret from "./secret";
+import { Routes, Route ,Link } from "react-router-dom";
 
 const Form = () => {
 
   const cookies = new Cookies();
-  
   const [username,setUsername]=useState(null);
   const[password,setPassword]=useState(null);
+  const[validation,setValidation]=useState(false);
+
   
     console.log(username,"...username ");
     console.log(password,"...password ");
 
-  return (
-    <>
-       <form>
-       <label> <h1 style={{display:"inline"}}>Enter your username:</h1> <input type="text" onChange={(e)=>{setUsername(e.target.value)}}/></label>
-       <br />
-       <label> <h1 style={{display:"inline"}}>Enter your password:</h1> <input type="text" onChange={(e)=>{setPassword(e.target.value)}}/></label>
-       <br />
-    </form>   
-
-       <button onClick={()=>{ 
-        let validationstate=false;
+    const handleAuthorization=()=>{
+      let validationstate=false;
         for(let i=0;i<userdetails.length;i++){
           if(userdetails[i].user==username){
             validationstate =true;
@@ -32,18 +25,46 @@ const Form = () => {
         console.log(validationstate,"....validationstate")
 
         if(validationstate){
-        console.log("you are authorized 👍")
-        cookies.set('user-set',username,{ path: '/' });
-        console.log(cookies.get('user-set'));
+            console.log("you are authorized 👍")
+            cookies.set('user-set',username,{ path: '/' });
+            console.log(cookies.get('user-set'),".....cookies ");
+            setValidation(true)
         }
         else{
-          console.log("you are not authorized 👎");
+            console.log("you are not authorized 👎");
             alert("you are not authorized 👎");
+            setValidation(false);
+        }
+
+        }
+
+        console.log(validation,".....this is validation")
+
+
+  return (
+    <>
+       <form>
+       <label> <h1 style={{display:"inline"}}>Enter your username:</h1> <input type="text" onChange={(e)=>{setUsername(e.target.value.toLowerCase())}}/></label>
+       <br />
+       <label> <h1 style={{display:"inline"}}>Enter your password:</h1> <input type="text" onChange={(e)=>{setPassword(e.target.value.toLowerCase())}}/></label>
+       <br />
+    </form>   
+
+       <button onClick={()=>handleAuthorization()}>Submit</button>
+
+        {
+          validation &&
+             <a href="./secret"><button>Click me</button></a>
         }
 
 
-        // handleValidation();
-        }}>Submit</button>
+        <Routes>
+           <Route path='/Secret' element={<Secret/>} /> 
+       </Routes> 
+        
+      
+
+
     </>
   )
 }
---
+++ src/components/secret.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Secret = () => {
+  return (
+    <>
+        <h1>This page can only be accessed by Front-end Devs , Congratulations !!</h1>
+        <a href="">You can play Snake game </a>
+    </>
+  )
+}
+
+export default Secret
